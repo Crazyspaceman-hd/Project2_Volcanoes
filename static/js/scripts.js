baabcd30ce225ba0bbe70132fd1d29f2441d85dd
Autumn Phenomenon <sharathmenon7500@gmail.com>
2021-04-30 21:38:29 -0700

    adding to the main

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,32 +1,59 @@
 // Empty JS for your own code to be here
-function getVolcano(number) {
-    d3.json('http://127.0.0.1:5000/api/volcanoes').then((data)=> {
-        console.log(data)
+function createMap(EruptionSpots) {
 
-    var volcanoes = data.volcano_number;
-    var volcanoName = data.volcano_name;
-    var country = data.country;
-    var elevation = data.elevation;
-    var eruptions = data.eruptions;
-    var vei = data.vei;
-    var volcanoType = data.primary_volcano_type;
-    var latitude = data.latitude;
-    var longitude = data.longitude;
+  // Create the tile layer that will be the background of our map
+  const lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 16,
+    id: "mapbox/light-v10",
+    accessToken: API_KEY
+  });
 
-    
-    console.log(volcanoes);
-    console.log(volcanoName);
-    console.log(country);
-    console.log(elevation);
-    console.log(eruptions);
-    console.log(vei);
-    console.log(volcanoType);
-    console.log(latitude);
-    console.log(longitude);
+  // Create a baseMaps object to hold the lightmap layer
+  const baseMap = {
+    "Light Map": lightmap
+  };
 
+  // Create an overlayMaps object to hold the EruptionSpots layer
+  const overlayMap = {
+    "Volcanoes": EruptionSpots
+  };
 
-})
+  // Create the map object with options
+  const map = L.map("map-points", {
+    center: [20.73, -174.0059],
+    zoom: 3,
+    layers: [lightmap, EruptionSpots]
+  });
 
+  // Create a layer control of basemaps and overlay maps
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(map);
 }
 
-getVolcano();
+function createPoints(response) {
+
+  // Pull the volcano property off of response.data
+  const volcanoes = response.eruptions;
+
+  // Initialize an array to hold bike markers
+  const volcanoArray = [];
+
+  // Loop through the volcano array
+  volcanoes.forEach(spot => {
+    // For each station, create a marker and bind a popup with the volcano's name
+    const VolcanoItem = L.marker([spot.latitude, spot.longitude])
+      .bindPopup("<h3>" + spot.volcano_name + "<h3><h3>Type: " + spot.primary_volcano_type + "</h3>");
+
+    // Add the item to the volcano array
+    VolcanoArray.push(VolcanoItem);
+  })
+
+  // Create a layer group made from the volcano array
+  createMap(L.layerGroup(VolcanoArray));
+}
+
+
+// Perform an API call 
+d3.json("http://127.0.0.1:5000/api/volcanoes").then(createPoints);
